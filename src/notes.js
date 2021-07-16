00034ec4a6b1f90cbaa68e707ed8771526ec15ef
Afif Zuhair <afifzuhair08@gmail.com>
2021-07-16 23:47:15 +0800

    Create notes objects, refactoring App function using fat arrow for Note component

---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        key: 1,
+        title: "Apple",
+        content: "Created by Steve Job",
+    },
+    {
+        key: 2,
+        title: "Tesla",
+        content: "Run by Elon Musk",
+    },
+    {
+        key: 3,
+        title: "Google",
+        content: "Managed by Peter",
+    },
+    {
+        key: 4,
+        title: "Amazon",
+        content: "Founded by Jeff Bezos",
+    },
+];
+
+export default notes;
